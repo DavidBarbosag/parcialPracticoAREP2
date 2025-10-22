509dd3037e25e2137b6ff754964ab942054ded53
unknown <david.barbosa-g@labinfo.is.escuelaing.edu.co>
2025-10-22 09:50:31 -0500

    simulacro

--- math-services/src/main/java/eci/edu/parcial2arep/controller/MathController.java
+++ math-services/src/main/java/eci/edu/parcial2arep/controller/MathController.java
@@ -1,4 +1,22 @@
 package eci.edu.parcial2arep.controller;
 
+import eci.edu.parcial2arep.service.MathService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController
 public class MathController {
+
+    private final MathService mathService;
+
+    public MathController(MathService mathService) {
+        this.mathService = mathService;
+    }
+
+    @GetMapping("/lucasseq")
+    public Map<Integer, Object> lucasSequence(@RequestParam int value) {
+        return mathService.lucasSequence(value);
+    }
 }
--- math-services/src/main/java/eci/edu/parcial2arep/service/MathService.java
+++ math-services/src/main/java/eci/edu/parcial2arep/service/MathService.java
@@ -1,4 +1,23 @@
 package eci.edu.parcial2arep.service;
 
+import java.util.Arrays;
+import java.util.Map;
+
 public class MathService {
+
+
+    public Map<String, Object> lucasSequence(int value) {
+
+        int l0 = 2;
+        int l1 = 1;
+        int acum = 0;
+        for (int i = 2; i < value; i++) {
+            if(value >= 2)
+                return Map.of("operation : Secuencia de Lucas", true, "value", i);
+
+        }
+        return Map.of("operation : Secuencia de Lucas", false, "value", -1);
+    }
+
+
 }
